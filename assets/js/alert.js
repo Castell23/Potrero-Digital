fe27e7959ad3a15a69c7ca4f5143496d4617b228
Emiliano Castell <gastoncastell98@gmail.com>
2021-10-21 22:12:11 -0300

    agregando dos alerts y mejorando la landing

---
+++ assets/js/alert.js
@@ -0,0 +1,5 @@
+alert("¿Esta dispuesto a ver el mejor sitio de recetas del mundo mundial?")
+
+function datosDelCreador() {
+    alert("Emiliano Castell, en conjunto con Luis Amarilla y Belén Bernal.")
+}
